db44575f3b3f329161ebe2942747e9216b8f4db2
Evgeniy Datskij <evgenij.datskij@mail.ru>
2023-05-30 15:04:29 +0500

    add category dor Categories

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,13 +1,22 @@
+import React from 'react';
+
 const Categories = () => {
+  const categorias = ['Все', 'Мясные', 'Вегетарианская', 'Гриль', 'Острые', 'Закрытые'];
+  const [active, setActive] = React.useState(0);
+
   return (
     <div className="categories">
       <ul>
-        <li className="active">Все</li>
-        <li>Мясные</li>
-        <li>Вегетарианская</li>
-        <li>Гриль</li>
-        <li>Острые</li>
-        <li>Закрытые</li>
+        {categorias.map((category, index) => {
+          return (
+            <li
+              onClick={() => setActive(index)}
+              key={index}
+              className={active === index ? 'active' : ''}>
+              {category}
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
