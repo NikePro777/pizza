f5128a8345e06eafa665341bfaa8bd8f37f1e5bf
Evgeniy Datskij <evgenij.datskij@mail.ru>
2023-06-23 22:37:54 +0500

    install router dom, start settings

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './scss/app.scss';
-import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+import Layout from './routes/layout';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+);
---
+++ src/routes/layout.jsx
@@ -0,0 +1,14 @@
+import Header from '../components/Header';
+
+const Layout = () => {
+  return (
+    <div className="wrapper">
+      <Header />
+      <div className="content">
+        <div className="container"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Layout;
